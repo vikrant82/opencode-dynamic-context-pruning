359e2781a6d70b743ed5335ec8b713c080a4e523
Daniel Smolsky <dannysmo@gmail.com>
2026-04-11 21:56:23 -0400

    format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["index.ts"],
-  format: ["esm"],
-  dts: false,
-  clean: true,
-  sourcemap: true,
-  noExternal: ["jsonc-parser"], // Bundle this to fix its broken ESM imports
-});
+    entry: ["index.ts"],
+    format: ["esm"],
+    dts: false,
+    clean: true,
+    sourcemap: true,
+    noExternal: ["jsonc-parser"], // Bundle this to fix its broken ESM imports
+})
